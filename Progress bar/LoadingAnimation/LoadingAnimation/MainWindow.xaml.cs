bc14ac4f5f3ff202d757aba2fe526e27531222b6
Shehryar Khan <shehryarkn@gmail.com>
2019-10-14 13:20:12 +0500

    Initial

---
+++ Progress bar/LoadingAnimation/LoadingAnimation/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoadingAnimation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker worker;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            long sum = 0;
+            long total = 100000;
+            for (long i = 1; i <= total; i++)
+            {
+                sum += i;
+                int percentage = Convert.ToInt32(((double)i / total) * 100);
+
+                Dispatcher.Invoke(new System.Action(() =>
+                {
+                    worker.ReportProgress(percentage);
+                }));
+            }
+            MessageBox.Show("Sum: " + sum);
+        }
+
+        void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            MyProgressBar.Value = e.ProgressPercentage;
+        }
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MyProgressBar.Visibility = Visibility.Collapsed;
+            MyProgressLabel.Visibility = Visibility.Collapsed;
+            PerformTask.IsEnabled = true;
+        }
+
+        private void PerformTask_Click(object sender, RoutedEventArgs e)
+        {
+            MyProgressBar.Visibility = Visibility.Visible; //Make Progressbar visible
+            MyProgressLabel.Visibility = Visibility.Visible; //Make TextBlock visible
+            PerformTask.IsEnabled = false; //Disabling the button
+            worker = new BackgroundWorker(); //Initializing the worker object
+            worker.ProgressChanged += Worker_ProgressChanged; //Binding Worker_ProgressChanged method
+            worker.DoWork += Worker_DoWork; //Binding Worker_DoWork method
+            worker.WorkerReportsProgress = true; //telling the worker that it supports reporting progress
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted; //Binding worker_RunWorkerCompleted method
+            worker.RunWorkerAsync(); //Executing the worker
+        }
+    }
+}
